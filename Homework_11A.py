d84f341ab6a4e4a6a14b15d87c6f38a600f6d628
Anna110009 <160776242+Anna110009@users.noreply.github.com>
2024-04-26 22:39:18 +0400

    Homework_11A.py

--- Homework_11A.py
+++ Homework_11A.py
@@ -12,34 +12,34 @@ dice_f = 6
 means = []
 p_values = []
 
-# Original distribution
+
 orig_dist = np.random.randint(1, dice_f + 1, size=num_trials * dice_r)
 orig_dist_means = np.mean(orig_dist.reshape(-1, dice_r), axis=1)
 
-# The Histogram of the means
+
 hist_ax = axs[0, 0]
 hist_ax.set_title('the histogram of the means')
 hist_ax.set_xlabel('mean')
 hist_ax.set_ylabel('frequency')
 
-# QQ plot
+
 qq_ax = axs[0, 1]
 qq_ax.set_title('QQ Plot')
 qq_ax.set_xlabel('theoretical quantiles')
 qq_ax.set_ylabel('ordered values')
 
-# Original distribution
+
 orig_dist_ax = plt.subplot2grid((2, 3), (0, 2), rowspan=2)
 orig_dist_ax.set_title('the original distribution')
 orig_dist_ax.set_xlabel('value')
 orig_dist_ax.set_ylabel('frequency')
 orig_dist_ax.yaxis.set_ticklabels([])
 
-# Shapiro-Wilk Test p-values
+
 dice_dist_ax =  axs[1, 0]
 dice_dist_ax.set_title('shapiro-Wilk test p-values')
 
-# Historical P-values 
+
 p_values_ax = axs[1, 1]
 p_values_ax.set_title('shapiro-wilk test p-values')
 p_values_ax.set_xlabel('trial')
@@ -53,7 +53,7 @@ def update(frame):
     dice_t = np.random.randint(1, dice_f + 1, size=dice_r)
     means.append(np.mean(dice_t))
     
-    # histogram of means
+    
     hist_ax.clear()
     hist_ax.hist(means, bins=6, range=(0, 7), rwidth=0.9)
     hist_ax.set_title('the Histogram of The Means')
@@ -62,14 +62,14 @@ def update(frame):
     hist_ax.set_xlim(0, 6)
     hist_ax.set_ylim(0, 20)
  
-    # QQ plot
+    
     qq_ax.clear()
     stats.probplot(means, dist="norm", plot=qq_ax)
     qq_ax.set_title('QQ Plot')
     qq_ax.set_xlabel('theoretical Quantiles')
     qq_ax.set_ylabel('sample quantiles')
 
-    # the original distribution plot
+    
     orig_dist_ax.clear()
     orig_dist_ax.hist(orig_dist_means[:frame], bins=6, range=(0, 7), rwidth=0.9)
     orig_dist_ax.set_title('the original distribution')
@@ -78,7 +78,7 @@ def update(frame):
     orig_dist_ax.set_xlim(0, 6)
     orig_dist_ax.set_ylim(0, 10)
     
-    # the Shapiro-Wilk test results
+    
     shapiro_statistic, p_value = stats.shapiro(dice_t)
     p_values.append(p_value)
       
@@ -87,7 +87,7 @@ def update(frame):
     dice_dist_ax.text(0.5, 0.5, f'P-values: {p_value:.3f}',fontsize=15, ha='center', va='center', transform=dice_dist_ax.transAxes)
     dice_dist_ax.axis('off')
     
-    # p-values plot
+    
     p_values_ax.clear()
     p_values_ax.plot(range(len(p_values)), p_values, marker='o', linestyle='-')
     p_values_ax.set_title('shapiro-wilk test p-values')
